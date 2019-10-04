610b80d8980c0dd7df870a6dc78b519620afe1a7
vPalomo <vPalomo@LP1S04.fuensalida.com>
2019-10-04 15:03:51 +0200

    mejora en la barra

--- Fichajes/src/com/iteku/backofficefichajes/VentanaCargarHorarios.java
+++ Fichajes/src/com/iteku/backofficefichajes/VentanaCargarHorarios.java
@@ -6,6 +6,7 @@
 package com.iteku.backofficefichajes;
 
 import com.iteku.utils.Utils;
+import java.awt.Window;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -49,6 +50,7 @@ public class VentanaCargarHorarios extends javax.swing.JPanel {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
+        jButton3 = new javax.swing.JButton();
 
         jButton1.setText("Examinar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -71,6 +73,14 @@ public class VentanaCargarHorarios extends javax.swing.JPanel {
         jLabel3.setText("de Cargar los datos de horarios.");
 
         jTextField1.setText("jTextField1");
+
+        jButton3.setText("Salir");
+        jButton3.setEnabled(false);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -102,6 +112,10 @@ public class VentanaCargarHorarios extends javax.swing.JPanel {
                         .addGap(169, 169, 169)
                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -121,13 +135,17 @@ public class VentanaCargarHorarios extends javax.swing.JPanel {
                 .addGap(18, 18, 18)
                 .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(21, 21, 21))
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(7, 7, 7)
+                .addComponent(jButton3)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         if(!jFileText.getText().equals("")){
+            jTextField1.setText("");
+            jButton2.setEnabled(false);
             try{
                 /*Process p = Runtime.getRuntime().exec("cmd /C dir");  
                 BufferedReader in = new BufferedReader(  
@@ -161,6 +179,7 @@ public class VentanaCargarHorarios extends javax.swing.JPanel {
                                                 System.out.println("FIN del hilo de la barra");
                                                 jProgressBar1.setValue(100);
                                                 jTextField1.setText("FIN");
+                                                jButton3.setEnabled(true);
                                             }
                                             if(Utils.isNumeric(linea)){
                                                 jProgressBar1.setValue(Integer.parseInt(linea));
@@ -204,10 +223,16 @@ public class VentanaCargarHorarios extends javax.swing.JPanel {
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        Window w = SwingUtilities.getWindowAncestor(this);
+            w.setVisible(false);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JTextField jFileText;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
