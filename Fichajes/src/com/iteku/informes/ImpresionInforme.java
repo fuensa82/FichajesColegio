65cd590ae7548e56033bf129d363ac5475b2c607
vPalomo <vPalomo@LP1S04.fuensalida.com>
2019-10-30 15:02:53 +0100

    Con guardado de informes. Cabeceras

--- Fichajes/src/com/iteku/informes/ImpresionInforme.java
+++ Fichajes/src/com/iteku/informes/ImpresionInforme.java
@@ -32,6 +32,10 @@ public class ImpresionInforme {
     private ArrayList<ProfesorBean> ListaProfesores;
     private ProfesorBean profesor;
     private int mes;
+    private static Font FuenteCabecera1=FontFactory.getFont("arial", 22, Font.UNDERLINE, BaseColor.BLACK);
+    private static Font FuenteCabecera2=FontFactory.getFont("arial", 18, Font.ITALIC, BaseColor.BLACK);
+    private static Font FuenteTextoNormal=FontFactory.getFont("arial", 14, Font.NORMAL, BaseColor.BLACK);
+    private static Font FuenteTextoNegrita=FontFactory.getFont("arial", 14, Font.BOLD, BaseColor.BLACK);
     
     public ImpresionInforme(ArrayList<ProfesorBean> ListaProfesores, int mes) {
         this.ListaProfesores=ListaProfesores;
@@ -44,21 +48,21 @@ public class ImpresionInforme {
     }
     
     private void cabecera(Document documento) throws DocumentException{
-        Paragraph p=new Paragraph("COLEGIO SAN JOSE",
-                                        FontFactory.getFont("arial",   // fuente
-                                        22,                            // tamaño
-                                        Font.UNDERLINE,
-                                        BaseColor.BLACK));
+        Paragraph p=new Paragraph("COLEGIO SAN JOSE",FuenteCabecera1);
         p.setAlignment(Chunk.ALIGN_CENTER);
         documento.add(p);
         documento.add(new Paragraph(" "));
-        documento.add(new Paragraph("Informe de horas del mes de "+FechasUtils.getMesNum(mes),
-                                        FontFactory.getFont("arial",   // fuente
-                                        18,                            // tamaño
-                                        Font.ITALIC,                   // estilo
-                                        BaseColor.BLACK)));             // color
+        documento.add(new Paragraph("Informe de horas del mes de "+FechasUtils.getMesNum(mes),FuenteCabecera2));         
+        documento.add( Chunk.NEWLINE );
+        Chunk c1 = new Chunk("Nombre y apellidos:  ", FuenteTextoNormal);
+        Chunk c2 = new Chunk(profesor.getNombre()+" "+profesor.getApellidos(),FuenteTextoNegrita);
+        Paragraph p2=new Paragraph();
+        p2.add(c1);
+        p2.add(c2);
+        documento.add(p2);
+        documento.add( Chunk.NEWLINE );
         documento.add(new Paragraph("Fecha de generación de los datos: "));
-        
+        documento.add( Chunk.NEWLINE );
         documento.add(new Paragraph("Fecha de generación del informe: "+FechasUtils.dameFechaNTP()));
     }
     
